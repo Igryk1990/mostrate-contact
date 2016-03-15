fb3a3f00cf105963a37a9ed7769968ca4d1abfd0
Igor <mustang90@bk.ru>
2016-03-15 23:01:43 +0300

    new.html

--- js/new.js
+++ js/new.js
@@ -1,23 +1,16 @@
-function progress()
-    {
-        var i=0;
-        var width= document.getElementById('bar-fill-two').parentNode.clientWidth;
-		var width= document.getElementById('bar-fill').parentNode.clientWidth;
-		var width= document.getElementById('bar-fill-three').parentNode.clientWidth;
-        var id=setInterval(grow, 10)
-        
-        function grow()
-        {
-            if(i<width)
-            {
-                i+=1;
-                if(!document.getElementById('bar-fill').setAttribute("style","width: "+i+"px;"))
-                    document.getElementById('bar-fill').style.width = i;
-                if(!document.getElementById('bar-fill-two').setAttribute("style","width: "+i+"px;"))
-                    document.getElementById('bar-fill-two').style.width = i;
-                if(!document.getElementById('bar-fill-three').setAttribute("style","width: "+i+"px;"))
-                    document.getElementById('bar-fill-three').style.width = i;
-                
-            }
-        }
-    }
+function clickOn() {
+var elem = document.getElementById("yes");
+elem.style.cssText = "position:fixed; color: red";
+var a = window.screen.width-100;
+var b = window.screen.heigth;
+elem.style.left = a + "px";
+elem.style.bottom = b + "px";
+};
+function clickON() {
+var elem = document.getElementById("no");
+elem.style.cssText = "position:fixed; color: red";
+var a = window.screen.width-100;
+var b = window.screen.heigth;
+elem.style.left = a + "px";
+elem.style.bottom = b + "px";
+};
